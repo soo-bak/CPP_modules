f6612b75abd098ee9e2166894662a29f97608e7b
jinchoi <znyous@gmail.com>
2021-07-31 06:34:52 +0900

    function devided

--- module_00/ex00/Main.cpp
+++ module_00/ex00/Main.cpp
@@ -1,35 +1,22 @@
-#include <cstring>
 #include <iostream>
 #include <string>
-#include <sstream>
 
-// int main(int argc, char* argv[]) {
-//   if (argc == 1) {
-//     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-//   } else {
-//     for (int i = 1; i < argc; i++) {
-//       int length = strlen(argv[i]);
-//       for (int j = 0; j < length; j++) {
-//         char upperCase = toupper(argv[i][j]);
-//         std::cout << upperCase;
-//       }
-//     }
-//   }
-//   std::cout << std::endl;
-//   return 0;
-// }
+const std::string Noise = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+
+std::string MakeUpperMessage(char *input) {
+  std::string message = static_cast<std::string>(input);
+  for (std::string::iterator j = message.begin(); j < message.end(); j++) {
+    *j = toupper(*j);
+  }
+  return message;
+}
 
 int main(int argc, char *argv[]) {
     if (argc == 1) {
-      std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+      std::cout << Noise;
     } else {
       for (int i = 1; i < argc; i++) {
-        std::string message;
-        message = argv[i];
-        for (std::string::iterator j = message.begin(); j < message.end(); j++) {
-          *j = toupper(*j);
-          std::cout << *j;
-        }
+        std::cout << MakeUpperMessage(argv[i]);
       }
     }
   std::cout << std::endl;
