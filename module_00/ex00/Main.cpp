49fe94d538d455884ca0956d41882a7f2a2e5eaa
jinchoi <znyous@gmail.com>
2021-07-31 05:32:42 +0900

    ex00_done

--- module_00/ex00/Main.cpp
+++ module_00/ex00/Main.cpp
@@ -1,12 +1,20 @@
+#include <cstring>
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[]) {
-  if (argc > 1) {
-    for (int i = 1; i < argc; i++) {
-      std::cout << argv[argc] << std::endl;
-    }
+
+  if (argc == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
   } else {
-    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    for (int i = 1; i < argc; i++) {
+      int length = strlen(argv[i]);
+      for (int j = 0; j < length; j++) {
+        char upperCase = toupper(argv[i][j]);
+        std::cout << upperCase;
+      }
+    }
   }
+  std::cout << std::endl;
   return 0;
 }
