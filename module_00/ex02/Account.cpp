b4f6c830111030438abda660aae93665cfb3bc91
jinchoi <znyous@gmail.com>
2021-08-05 03:00:44 +0900

    making timestam is done. in Account.cpp

--- module_00/ex02/Account.cpp
+++ module_00/ex02/Account.cpp
@@ -36,11 +36,16 @@ void Account::_displayTimestamp() {
   const int thisYear = 1900 + localTimer->tm_year;
   const int thisMonth = 1 + localTimer->tm_mon;
   const int thisDay = localTimer->tm_mday;
+  const int thisHour = localTimer->tm_hour;
+  const int thisMinute = localTimer->tm_min;
+  const int thisSecond = localTimer->tm_sec;
   std::stringstream stringStream;
   stringStream << "[" << thisYear;
   stringStream << std::setfill('0') << std::setw(2) << thisMonth;
-  stringStream << std::setfill('0') << std::setw(2) << thisDay << "]";
-  std::string timeStamp = stringStream.str();
-
+  stringStream << std::setfill('0') << std::setw(2) << thisDay << "_";
+  stringStream << std::setfill('0') << std::setw(2) << thisHour;
+  stringStream << std::setfill('0') << std::setw(2) << thisMinute;
+  stringStream << std::setfill('0') << std::setw(2) << thisSecond << "]";
+  const std::string timeStamp = stringStream.str();
   std::cout << timeStamp << std::endl;
 }
