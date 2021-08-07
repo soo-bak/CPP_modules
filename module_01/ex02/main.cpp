777cd4c0cafd7cabce481765b873b394b66abf7b
jinchoi <znyous@gmail.com>
2021-08-07 11:33:59 +0900

    module 01 ex02 ansi code added

--- module_01/ex02/main.cpp
+++ module_01/ex02/main.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 
+
+const std::string g_colorRed("\033[1;31m");
+const std::string g_colorPurple("\033[1;35m");
+const std::string g_colorEnd("\033[0m");
+
 void testScope(std::string* pointer){
-  const std::string colorStart("\033[1;31m");
-  const std::string colorEnd("\033[0m");
-  std::cout << colorStart;
-  std::cout << "<Pointer>   address[" << &pointer << "]";
-  std::cout << " dereferencing value[" << *pointer << "]" << std::endl;
-  std::cout << colorEnd;
+  std::cout << g_colorPurple;
+  std::cout << std::setw(16) << std::left;
+  std::cout << "<arg Pointer>" << "address[" << &pointer << "]";
+  std::cout << g_colorEnd;
+  std::cout << " : " << *pointer << std::endl;
 }
 
 void testScope(std::string& reference){
-  const std::string colorStart("\033[1;31m");
-  const std::string colorEnd("\033[0m");
-  std::cout << colorStart;
-  std::cout << "<Reference> address[" << &reference << "]";
-  std::cout << " dereferencing value[" << reference << "]" << std::endl;
-  std::cout << colorEnd;
+  std::cout << std::setw(16) << std::left;
+  std::cout << "<arg Reference>" << "address[" << &reference << "]";
+  std::cout << " : " << reference << std::endl;
 }
 
 int main () {
+
   std::string brain = "HI THIS IS BRAIN";
   std::string* stringPTR(&brain);
   std::string& stringREF(brain);
-  std::cout << "<Origin>    address[" << &brain << "]" << std::endl;
-  std::cout << "<Pointer>   address[" << &stringPTR << "]";
-  std::cout << " dereferencing value[" << *stringPTR << "]" << std::endl;
-  std::cout << "<Reference> address[" << &stringREF << "]";
-  std::cout << " dereferencing value[" << stringREF << "]" << std::endl;
+  std::cout << std::setw(16) << std::left;
+  std::cout << "<Origin>" << "address[" << &brain << "]";
+  std::cout << " : " << brain << std::endl;
+  std::cout << g_colorRed;
+  std::cout << std::setw(16) << std::left;
+  std::cout << "<Pointer>" << "address[" << &stringPTR << "]";
+  std::cout << g_colorEnd;
+  std::cout << " : " << *stringPTR << std::endl;
+  std::cout << std::setw(16) << std::left;
+  std::cout << "<Reference>" << "address[" << &stringREF << "]";
+  std::cout << " : " << stringREF << std::endl;
   testScope(stringPTR);
   testScope(stringREF);
   return 0;
