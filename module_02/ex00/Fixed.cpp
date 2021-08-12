1a442d7da8cea2ba01cd4f2aba4048d6e26ad29e
jinchoi <znyous@gmail.com>
2021-08-12 13:32:01 +0900

    code style trimed

--- module_02/ex00/Fixed.cpp
+++ module_02/ex00/Fixed.cpp
@@ -7,13 +7,13 @@ Fixed::Fixed(void)
   std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::~Fixed(void) {
-  std::cout << "Destructor called" << std::endl;
-}
-
 Fixed::Fixed(const Fixed& fixed) {
   std::cout << "Copy constructor called" << std::endl;
   *this = fixed;
+}
+
+Fixed::~Fixed(void) {
+  std::cout << "Destructor called" << std::endl;
 }
 
 Fixed& Fixed::operator = (const Fixed& source) {
--- module_02/ex00/Fixed.hpp
+++ module_02/ex00/Fixed.hpp
@@ -5,16 +5,16 @@
 class Fixed {
   public:
     Fixed(void);
-    ~Fixed(void);
     Fixed(const Fixed& fixed);
+    ~Fixed(void);
 
     Fixed& operator = (const Fixed& source);
 
     int getRawBits(void) const;
     void setRawBits(int const raw);
 
-
   private:
     static const int _fractionalBitsNumber;
+
     int _rawBits;
 };
