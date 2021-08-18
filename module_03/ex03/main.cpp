72223ee547cd0ad73627ab515bfab705b2f43c01
jinchoi <znyous@gmail.com>
2021-08-18 14:38:30 +0900

    module03 ex03 mian.cpp modified

--- module_03/ex03/main.cpp
+++ module_03/ex03/main.cpp
@@ -5,7 +5,15 @@
 int main() {
   DiamondTrap diamondTrap("D14M0ND-TP");
 
+  diamondTrap.displayStatus();
+  std::cout << std::endl;
 
+  diamondTrap.attack("5H4D0W-TP");
+  diamondTrap.displayStatus();
+  std::cout << std::endl;
+
+  diamondTrap.whoAmI();
+  std::cout << std::endl;
 
   return 0;
 }
