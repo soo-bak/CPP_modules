4492295f54f4241f2c7be492544173e171cb35fa
jinchoi <znyous@gmail.com>
2021-08-19 20:26:31 +0900

    module04 ex00 Animal class is maded

---
+++ module_04/ex00/Animal.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Animal {
+  public:
+    Animal(void);
+    Animal(const std::string& type);
+    Animal(const Animal& animal);
+    virtual ~Animal();
+
+    Animal& operator = (const Animal& other);
+
+    std::string getType(void) const;
+    void setType(const std::string& type);
+    virtual void makeSound(void) const;
+
+  protected:
+    std::string _type;
+};
---
+++ module_04/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "Animal.hpp"
+
+int main() {
+  const Animal* meta = new Animal();
+
+
+  meta->makeSound();
+
+  delete meta;
+  return 0;
+}
