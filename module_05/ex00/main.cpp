a15ae9a213ba58445f93a964c83a8f2461776d65
jinchoi <znyous@gmail.com>
2021-08-21 18:57:49 +0900

    module05 ex00 init

---
+++ module_05/ex00/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+const std::string ansiRed("\033[1;31m");
+const std::string ansiPurple("\033[1;35m");
+const std::string ansiBlue("\033[1;34m");
+const std::string asniCyan("\033[1;36m");
+const std::string ansiEnd("\033[0m");
+
+int main () {
+
+}
